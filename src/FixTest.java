0c418a6103a520c47aeaad86c2b814898bf5e827
REDFRONT\mleuhina <lolglebik@gmail.com>
2017-09-13 19:01:56 +0300

    30% completed

--- src/FixTest.java
+++ src/FixTest.java
@@ -1,21 +1,46 @@
+import com.codeborne.selenide.CollectionCondition;
 import org.junit.Test;
+import org.testng.Assert;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class FixTest {
     @Test
     public void userCanLoginByUsername() {
         String login = "QWA";
         String pass = "12345";
+        String val1 = "1";
+        String val2 = "2";
+        String val3 = "3";
         PageObjects page = new PageObjects();
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         System.setProperty("selenide.browser", "Chrome");
         open("http://rulkov.ru/qa/");
+        $(page.welcome).isDisplayed();
+        $$(page.allImg).shouldHave(CollectionCondition.size(11));
         $(page.headerLogin).click();
+        Assert.assertEquals(url(), "http://rulkov.ru/qa/login");
+        $(page.btnLogin).has(cssClass("btn-danger"));
         $(page.loginInput).setValue(login);
-        $(page.passwordInput).setValue(pass).pressEnter();
+        $(page.passwordInput).setValue(pass);
+        $(page.btnLogin).shouldNotHave(cssClass("btn-danger"));
+        $(page.btnLogin).click();
         //$(".loading_progress").should(disappear); // Waits until element disappears
+        $(page.additionTitle).isDisplayed();
         $(page.greeting).shouldHave(text("Привет, " + login +"!")); // Waits until element gets text
+        $(page.descriptionTitle).shouldHave(text("Шаг \"Ввод данных\"."));
+        $(page.description).shouldHave(text("Содержит три текстовых поля, обязательных к заполнению, формат вводимых данных - целые положительные числа."));
+        Assert.assertEquals(url(), "http://rulkov.ru/qa/addition");
+        $(page.btnBack).shouldBe(disabled);
+        $(page.btnForward).shouldBe(disabled);
+        $(page.input1).sendKeys(val1);
+        $(page.input2).sendKeys(val2);
+        $(page.input3).sendKeys(val3);
+        $(page.btnForward).shouldNotBe(disabled);
+        $(page.btnForward).click();
+        $(page.descriptionTitle).shouldHave(text("Шаг \"Подтверждение данных\""));
+        $(page.description).shouldHave(text("На данном шаге, пользователю в табличном виде представляются данные, которые он ввел на шаге \"Ввода данных\"."));
     }
 }
--- src/PageObjects.java
+++ src/PageObjects.java
@@ -2,22 +2,42 @@ import org.openqa.selenium.By;
 
 public class PageObjects {
 
+    //Header
     public By headerLogin = By.xpath("//a[text()='Login']");
 
     public By headerLogout = By.xpath("//a[text()='logout']");
 
     public By headerAddition = By.xpath("//a[text()='Операция сложения']");
 
+    //Main page
+    public By welcome = By.xpath("//h1[text()='Welcome']");
+
+    public By allImg = By.xpath("//img");
+
+    //Login page
     public By loginInput = By.id("username");
 
     public By passwordInput = By.id("password");
 
+    public By btnLogin = By.xpath("//button[text()='Login']");
+
+    //Addition page
+    public By additionTitle = By.xpath("//h1[text()='Операция сложения']");
+
     public By greeting = By.xpath("//h3[contains(text(), 'Привет, ')]");
+
+    public By descriptionTitle = By.xpath("//h4");
+
+    public By description = By.xpath("//div[@class='animate-switch ng-scope']");
 
     public By input1 = By.id("value1");
 
     public By input2 = By.id("value2");
 
     public By input3 = By.id("value3");
+
+    public By btnBack = By.xpath("//input[@value='Назад']");
+
+    public By btnForward = By.xpath("//input[@value='Вперед']");
 
 }
